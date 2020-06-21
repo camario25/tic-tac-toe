67ada0ff323298ddbcb791573b35b36f245464c5
Mario Alcantar <malcantar@linkedin.com>
2020-06-21 12:19:47 -0700

    changed grid data structure to array of objects for more flexibility, made notes of functions that I will need for game

--- app/controllers/playgame.js
+++ app/controllers/playgame.js
@@ -1,4 +1,8 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
+import { action } from "@ember/object";
 
 export default class PlaygameController extends Controller {
+  constructor() {
+    super(...arguments);
+  }
 }
--- app/routes/playgame.js
+++ app/routes/playgame.js
@@ -4,9 +4,9 @@ export default class PlaygameRoute extends Route {
   model() {
     return {
       grid: [
-        ["x", "x", "x"],
-        ["", "", ""],
-        ["", "", ""],
+        [{}, {}, {}],
+        [{}, {}, {}],
+        [{}, {}, {}],
       ],
       players: [
         {
