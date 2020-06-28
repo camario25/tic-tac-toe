105fc00590dff17a228e6e55735544db34175e08
Mario Alcantar <malcantar@linkedin.com>
2020-06-28 16:00:37 -0700

    added more documentation to JS, added favicon, and added more accessibility for screen readers

--- app/controllers/playgame.js
+++ app/controllers/playgame.js
@@ -16,36 +16,54 @@ export default class PlaygameController extends Controller {
     this.userView = "playerNames"; //game starts with asking for player names
   }
 
-  @tracked userView;
-  @tracked currentMarker;
-  @tracked currentName;
-  @tracked playerA;
-  @tracked playerB;
-  @tracked draw;
-  @tracked winner;
+  @tracked userView; //{string} used to decide what to render to the user
+  @tracked currentMarker; //{string} current marker either "x" or "o"
+  @tracked currentName; //{string} Name of player who's move it is
+  @tracked playerA; //{string} Name of player from first input
+  @tracked playerB; //{string} Name of player from second input
+  @tracked draw; //{boolean} true if no more spaces and no winner
+  @tracked winner; //{string} name of winner
 
+  /**
+   * creates a nested array with as many arrays as size and as many objects in
+   * the arrays as size and attatches the grid to the model so it can be
+   * rendered in the templates
+   * @param {Number} size This number is flexible and will create an NxN grid,
+   * later it will be called with 3 for tictactoe
+   * @returns {undefined}
+   */
   setGrid(size) {
     this.set("model.grid", createGrid(size));
   }
 
-  addWinToPlayer(players, name) {
-    players.find((player) => player.name === name);
-  }
-
+  /**
+   * assigns the value in text input field to this.playerA in Controller
+   * @param {Object} e Dom input event
+   */
   @action
   inputPlayerA(e) {
     this.playerA = e.target.value;
   }
 
+  /**
+   * assigns the value in text input field to this.playerB in Controller
+   * @param {Object} e DOM input event
+   */
   @action
   inputPlayerB(e) {
     this.playerB = e.target.value;
   }
 
+  /**
+   * Action is called when user submits the player names this action sets state
+   * on the controller and sets the names and markers for the players in the
+   * model
+   * @param {Object} e DOM submit event
+   */
   @action
   newGame(e) {
     e.preventDefault();
-    this.userView = "playingGame";
+    this.userView = "playingGame"; //reveals the grid and player sections
     this.winner = null;
     this.draw = null;
     this.currentMarker = "x"; // first move is x
@@ -62,9 +80,17 @@ export default class PlaygameController extends Controller {
       set(this.model.players[0], "marker", "o");
       this.currentName = this.playerB;
     }
+    //actually creates the 3x3 grid
+    //setGrid will respond to any size but currently set to 3
     this.setGrid(3);
   }
 
+  /**
+   * resets wins in the model for both playes to 0 setting this.userView to
+   * "playerNames" which renders the form to ask for new player names so they can be
+   * used to reassign.
+   * Action is called when users want to play with new names
+   */
   @action
   newPlayers() {
     set(this.model.players[0], "wins", 0);
@@ -72,24 +98,32 @@ export default class PlaygameController extends Controller {
     this.userView = "playerNames";
   }
 
-  @action
-  hoverInMarker(position) {
-    console.log("in");
-  }
-
-  @action
-  hoverOutMarker(position) {
-    console.log("out");
-  }
-
+  /**
+   * Action fired when user places a marker on the grid Each object representing
+   * a square on the grid was assinged an attribute position.
+   * @param {Array.<Number>} position an array length of 2 each a number, either
+   * 0 or 1.  Essentially this is the ID of the square, if we didn't use
+   * position another method is passing some unique id to identify the square.
+   * In which case I would need some sort of map from id to position in array.
+   * Position is array of two numbers that represent the position of the square
+   * in the grid using index position. This allows us to take that information
+   * on the click event and pass it the function to change the value of that
+   * position in the model. The model is only accessed using index position and
+   * that is why we are doing this.
+   */
   @action
   makeMove(position) {
+    /**First the square's marker attribute in the model is changed to the
+     *currentMarker.
+     */
     set(
       this.model.grid[position[0]][position[1]],
       "marker",
       this.currentMarker
     );
     if (isWinner(this.model.grid)) {
+      //check for a winner
+      //If true the winners wins attribute in the model increases by one
       this.winner = this.currentName;
       const WIN_INCREMENT = 1;
       if (this.model.players[0].name === this.winner) {
@@ -105,11 +139,13 @@ export default class PlaygameController extends Controller {
           this.model.players[1].wins + WIN_INCREMENT
         );
       }
-      this.userView = "endGame";
+      this.userView = "endGame"; //view goes to end game on win
     } else if (!stillHasSpaces(this.model.grid)) {
+      //check to see if there are more playable spaces
       this.draw = true;
-      this.userView = "endGame";
+      this.userView = "endGame"; //view goes to end game on draw
     } else {
+      //if more free spaces, next turn, we update currentMarker and CurrentName
       this.currentMarker = getCurrentMarker(this.currentMarker);
       this.currentName = getCurrentName(
         this.currentName,
--- app/utils/tictactoe.js
+++ app/utils/tictactoe.js
@@ -1,19 +1,27 @@
-/*Returns an array that represents a row, eventually used in the grid
-@param {Number} size The length of one row in the grid, for 3x3 grid, size is 3
-@param {Number} counter This number represents the eventual position of the row in the grid, it increments at a different rate than the loop hence why is used later in createGrid(size);
-@return {Array} The array of objects representing "squares" in a row.  Each array is a row in the grid.
-*/
+/**
+ * Returns an array that represents a row, eventually used in the grid
+ * @param {Number} size The length of one row in the grid, for 3x3 grid, size is
+ * 3
+ * @param {Number} counter This number represents the eventual position of the
+ * row in the grid, it increments at a different rate than the loop hence why is
+ * used later in createGrid(size);
+ * @returns {Array.<Object>} The array of objects representing "squares" in a
+ * row.  Each array is a row in the grid.
+ */
 function createRow(size, counter) {
   return [...new Array(size)].map((el, i) => {
     return { marker: "", position: [counter, i] };
   });
 }
 
-/*Creates a grid from rows made by createRow(size, counter);
-@param {Number} size The height of the grid, or how many rows in the grid.  For example a 3x3 grid, size would be 3
-@return {Array} Returns a nested Array with however many arrays inside as the number size
-*/
-
+/**
+ * Creates a grid from rows made by createRow(size, counter);
+ * @param {Number} size The height of the grid, or how many rows in the grid.
+ * For example a 3x3 grid, size would be 3
+ * @returns {Array.<Array.<Object>>} Returns a nested Array with however many
+ * arrays inside as the number size. The quantity of objects inside each array
+ * also matches size.
+ */
 function createGrid(size) {
   let counter = -1;
   return [...new Array(size)].map(() => {
@@ -22,9 +30,20 @@ function createGrid(size) {
   });
 }
 
-//Dertirmine Winner * only for three in a row for now
-
+/**
+ * Dertirmines if there's a  Winner by checking if object value marker is not
+ * empty and equal to "x" or "o" 3 times in a row in any direction.  Currently
+ * only checks a 3x3 grid
+ * @param {Array.<Array.<Object>>} grid Two dimensional array with objects
+ * representing a tictactoe square
+ * @returns {boolean} True is there is a winner (not empty and 3 in row that
+ * match) or False is these conditions are not met.
+ */
 function isWinner(grid) {
+  /**
+   * @property {string} marker property on the object representing the tictactoe
+   * square. Values are either "","x", or "o".
+   */
   if (
     grid[0][0].marker !== "" &&
     grid[0][0].marker === grid[0][1].marker &&
@@ -78,26 +97,79 @@ function isWinner(grid) {
   }
 }
 
-//Check if board still has spaces
-
+/**
+ * Check if grid still has empty spaces (equal to ""). This allows us to know
+ * continue play or if we have a draw.
+ * @param {Array.<Array.<Object>>} grid Two dimensional array with objects
+ * representing a tictactoe square
+ * @returns {boolean} True if there is at least one square equal to "".  false
+ * otherwise.
+ */
 function stillHasSpaces(grid) {
-  return grid
-    .map((row) => row)
-    .some((row) => {
-      return row.some((square) => square.marker === "");
-    });
+  /**
+   * 1st some call to enter grid and allow iteration of inner arrays, accepts
+   * callback some returns a boolean if callback returns true once. accepts
+   * callback
+   * @param {anonymousCallBack1} cb1 callback to check truthiness (in this case
+   * it's for each array in grid)
+   * @returns {Array} unmutated array inside grid
+   * @returns {boolean} true if callback returns true
+   */
+  /**
+   * @callback cb1
+   * @param {Array} row Array of objects representing rows in the grid
+   */
+  return grid.some((row) => {
+    /**
+     * 2nd some call to check if value object.marker equals empty string
+     * @param {anonymousCallBack2} cb2 callback to check truthiness
+     * @returns {boolean} true if property marker in object square equals empty
+     * string
+     */
+    /**
+     * @callback cb2
+     * @param {Object} square Object containing the marker property where we set
+     * the value of the square.
+     * @property {string} square.marker value of square we set it to "x" or "o"
+     * defaults to "".
+     * @returns {boolean} As soon as square.marker equals "" once, returns true
+     * to the entire stillHasSpaces function, else returns false.
+     */
+    return row.some((square) => square.marker === "");
+  });
 }
 
-//Taking Turns, changes currentMarker from x to o or vice verca
-
+/**
+ * Players taking turns, changes current marker from "x" to "o" or vice verca
+ * @param {string} marker takes the current marker "x" or "o"
+ * @returns {string} Returns the opposite of marker, "x" or "o"
+ */
 function getCurrentMarker(marker) {
   return marker === "x" ? "o" : "x";
 }
 
+/**
+ * Players taking turns, returns the name of the player who's turn it is next
+ * after previous player moved (placed marker) note this function is called to
+ * reassign the current player name and so will usually be called after current
+ * player has physically placed a marker and win and draw validations are
+ * complete
+ * @param {string} currentName name of player who needs to move (place marker)
+ * @param {string} playerA name of player assinged to this param
+ * @param {string} playerB name of player assinged to this param
+ * @returns {string} Name of player who does not equal currentName
+ */
 function getCurrentName(currentName, playerA, playerB) {
   return currentName === playerA ? playerB : playerA;
 }
 
+/**
+ * uses Math.random() api which returns a float between 0 and .9 repeating to
+ * give a 50 50 chance of returning an "x" or an "o".  Used to select the player
+ * who goes first at random.
+ * @returns {string} "x" if Math.random() returns less than .5 or "o" if
+ * otherwise
+ */
 function selectFirstPlayer() {
   const FIFTY_PERCENT_CHANCE = 0.5;
   return Math.random() < FIFTY_PERCENT_CHANCE ? "x" : "o";
